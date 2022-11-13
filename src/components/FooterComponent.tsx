610e6b3c2fa8a3bf9a4c48a484d31492ebd90bdd
sammy-code98 <sammysmart98@yahoo.com>
2022-11-13 14:39:23 +0100

    add footer component

--- src/components/FooterComponent.tsx
+++ src/components/FooterComponent.tsx
@@ -1,7 +1,20 @@
 import React from 'react'
-
+import { Flex, Text, Link } from "@chakra-ui/react"
 export default function FooterComponent() {
     return (
-        <div>FooterComponent</div>
+        <Flex
+            w='full'
+            bg="blackAlpha.50"
+            minHeight='20vh'
+            flexDirection='column'
+            alignItems='center'
+            textAlign='center'
+            justifyContent='center'>
+            <Text mb='3'>
+                Built with love {' '}
+                <Link href='https://github.com/sammy-code98' isExternal color='blue.500'>CodeSammy98</Link>
+            </Text>
+            <Text opacity='0.5' px='4'>Open source landing page sample built with chakra ui</Text>
+        </Flex>
     )
 }
